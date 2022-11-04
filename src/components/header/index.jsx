23e3dffb2df6b6d881d0d7ee7742d871c217dec7
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-11-04 16:09:49 -0300

    header only logo

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from "react";
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className=" bg-white flex items-center justify-around">
+      <img
+        className="h-56 w-56"
+        src="https://res.cloudinary.com/dfbxjt69z/image/upload/v1667588311/delTomate/1_toejze.png"
+        alt=""
+      />
+    </div>
+  );
 }
