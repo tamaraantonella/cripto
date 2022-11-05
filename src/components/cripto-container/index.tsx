a710e9a04f54bf7cc801adca1919f0979c587a0e
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-11-05 14:33:06 -0300

    fetch api

--- src/components/cripto-container/index.tsx
+++ src/components/cripto-container/index.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import axios from "axios";
 
 export default function CriptoContainer() {
-  return (
-    <div>CriptoContainer</div>
-  )
+  const getCoins = async () => {
+    try {
+      let response = await axios.get(
+        "https://pro-api.coinmarketcap.com/v1/cryptocurrency/map",
+        {
+          headers: {
+            "X-CMC_PRO_API_KEY": "2231bb0f-1ade-4b96-8eee-d998ce0f33cf",
+          },
+        }
+      );
+      const { data } = response;
+      const responseArray = data.data;
+      console.log(responseArray);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getCoins();
+  }, []);
+  return <div>CriptoContainer</div>;
 }
