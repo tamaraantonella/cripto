b814da699e138ea2433360f25ad828a9710bbeaf
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-12-10 15:48:22 -0300

    Add renderCoins

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -13,15 +13,13 @@ export default function Container() {
   const indexOfLastCoin = currentPage * coinsPerPage;
   const indexOfFirstCoin = indexOfLastCoin - coinsPerPage;
   const currentCoins = coins?.slice(indexOfFirstCoin, indexOfLastCoin);
-
+  const renderCoins = (): JSX.Element[] => {
+    return currentCoins?.map((coin: any) => <Card key={coin.id} card={coin} />);
+  };
   return (
     <div className="mx-auto my-20 px-5 w-full">
       <Pagination currentPage={currentPage} setPage={setPage} />
-      <div className="grid grid-cols-4 ">
-        {currentCoins?.map((coin: any) => (
-          <Card key={coin.id} card={coin} />
-        ))}
-      </div>
+      <div className="grid grid-cols-4 ">{renderCoins()}</div>
     </div>
   );
 }
