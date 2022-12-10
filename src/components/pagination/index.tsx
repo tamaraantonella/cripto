f551a51156dcecf25b4712463de88edddf2d44e1
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-12-10 15:07:53 -0300

    Add react icons & pagination icons

--- src/components/pagination/index.tsx
+++ src/components/pagination/index.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import {
+  MdOutlineKeyboardArrowRight,
+  MdOutlineKeyboardArrowLeft,
+} from "react-icons/md";
 interface PaginationProps {
   currentPage: number;
   setPage: (page: number) => void;
@@ -11,12 +15,23 @@ export default function Pagination({ currentPage, setPage }: PaginationProps) {
   const handlePrevPage = () => {
     setPage(currentPage - 1);
   };
+  const maxPage = 9;
 
   return (
-    <div className="flex gap-3 justify-center text-2xl">
-      {currentPage > 1 && <button onClick={handlePrevPage}>-</button>}
-      <p>{currentPage}</p>
-      <button onClick={handleNextPage}>+</button>
+    <div className="grid grid-cols-3 w-fit mx-auto gap-3 justify-center font-semibold items-center text-violet-900 my-8">
+      <button
+        className={`px-3 text-2xl ${currentPage > 1 ? "visible" : ' invisible '}`}
+        onClick={handlePrevPage}>
+        <MdOutlineKeyboardArrowLeft />
+      </button>
+      <p className={`text-2xl `}>
+        {currentPage}
+      </p>
+      <button
+        className={`px-3 text-2xl ${currentPage < maxPage ? "visible" : 'invisible'}`}
+        onClick={handleNextPage}>
+        <MdOutlineKeyboardArrowRight />
+      </button>
     </div>
   );
 }
